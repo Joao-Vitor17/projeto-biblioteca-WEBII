a1a35c8ce7c6151d844589d0bd0a8dc47f8d4b84
Joao Vitor <8ballpoll.fake5@gmail.com>
2025-06-24 21:26:07 -0300

    feat: adiciona rotas protegidas por middleware para admin e aluno

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\AlunoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', AdminMiddleware::class])->group(function () {
+    Route::get('/home', function () {
+        return view('dashboard-admin');
+    })->name('home');
+});
+
+Route::middleware(['auth', AlunoMiddleware::class])->group(function () {
+    Route::get('/home', function () {
+        return view('dashboard-aluno');
+    })->name('home');
 });
 
 require __DIR__.'/auth.php';
