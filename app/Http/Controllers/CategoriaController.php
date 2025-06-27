a043dae56ebabe6c0e3949b7f90375b6eb6e3523
Joao Vitor <8ballpoll.fake5@gmail.com>
2025-06-26 21:08:23 -0300

    feat: adiciona controller Categoria com métodos de criação e listagem

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return view('categorias.index')->with('categorias', $categorias);
+    }
+
+    public function create()
+    {
+        return view('categorias.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'descricao' => 'required|string|max:255',
+        ]);
+
+        Categoria::create([
+            'nome' => $request->nome,
+            'descricao' => $request->descricao,
+        ]);
+
+        return redirect()->route('categorias.index')->with(['success' => 'Categoria criada com sucesso!!']);
+    }
+    
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
