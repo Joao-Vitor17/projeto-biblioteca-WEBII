8b037735cda6bcc0d388ad3d78f2ce13f3f1bb7f
Joao Vitor <8ballpoll.fake5@gmail.com>
2025-06-30 18:26:30 -0300

    feat: adiciona método de exclusão na controller do autor

--- app/Http/Controllers/AutorController.php
+++ app/Http/Controllers/AutorController.php
@@ -45,16 +45,31 @@ class AutorController extends Controller
 
     public function edit(string $id)
     {
-        //
+        $autor = Autor::find($id);
+        $dataAtual = Carbon::now()->format('Y-m-d');
+        return view('autores.edit')->with(['autor' => $autor, 'dataAtual' => $dataAtual]);
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'nacionalidade' => 'required|string|max:255',
+            'data_nascimento' => 'required|date|before_or_equal:today',
+        ]);
+
+        Autor::find($id)->update([
+            'nome' => $request->nome,
+            'nacionalidade' => $request->nacionalidade,
+            'data_nascimento' => $request->data_nascimento,
+        ]);
+
+        return redirect()->route('autores.index')->with(['success' => 'Autor atualizado com sucesso!!']);
     }
 
     public function destroy(string $id)
     {
-        //
+        Autor::destroy($id);
+        return redirect()->route('autores.index')->with(['success' => 'Autor excluído com sucesso!!']);
     }
 }
