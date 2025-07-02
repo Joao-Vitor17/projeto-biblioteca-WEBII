c65bcfb7ffd8a95ebcca0e392601ce9795905a98
Joao Vitor <8ballpoll.fake5@gmail.com>
2025-07-01 21:53:42 -0300

    feat: adiciona métodos de exibição, edição e exclusão na controller de livro

--- app/Http/Controllers/LivroController.php
+++ app/Http/Controllers/LivroController.php
@@ -45,21 +45,42 @@ class LivroController extends Controller
 
     public function show(string $id)
     {
-        //
+        $livro = Livro::find($id);
+        return view('livros.show')->with(['livro' => $livro, 'autores' => $livro->autores]);
     }
 
     public function edit(string $id)
     {
-        //
+        $livro = Livro::find($id);
+        $categorias = Categoria::all();
+        return view('livros.edit')->with(['livro' => $livro, 'categorias' => $categorias]);
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'titulo' => 'required|string|max:255',
+            'ano_publicacao' => 'required|integer|digits:4|max:'.date('Y'),
+            'estoque' => 'required|integer|min:1|max:999',
+            'categoria_id' => [
+                'required',
+                Rule::exists('categorias', 'id')->whereNull('deleted_at')
+            ]
+        ]);
+
+        Livro::find($id)->update([
+            'titulo' => $request->titulo,
+            'ano_publicacao' => $request->ano_publicacao,
+            'estoque' => $request->estoque,
+            'categoria_id' => $request->categoria_id,
+        ]);
+
+        return redirect()->route('livros.index')->with(['success' => 'Livro atualizado com sucesso!!']);
     }
 
     public function destroy(string $id)
     {
-        //
+        Livro::destroy($id);
+        return redirect()->route('livros.index')->with(['success' => 'Livro excluído com sucesso!!']);
     }
 }
