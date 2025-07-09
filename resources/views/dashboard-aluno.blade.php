20ccef386eb928073647d99a1ae6441639796b8a
Joao Vitor <8ballpoll.fake5@gmail.com>
2025-07-08 22:10:24 -0300

    feat: adiciona botões de gerenciamento de empréstimos e livros na home do aluno

--- resources/views/dashboard-aluno.blade.php
+++ resources/views/dashboard-aluno.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Tela Inicial')
 
 @section('content')
-    <h1 class="mb-4">Bem-vindo, {{ Auth::user()->name }}</h1>
-    <button class="btn btn-primary">Clique aqui, aluno!</button>
+    <div class="d-flex flex-column gap-2">
+        <h1 class="mb-4">Bem-vindo, {{ Auth::user()->name }}</h1>
+        <a href="{{ route('emprestimos.index') }}" class="btn btn-primary align-self-start">Visualizar Empréstimos</a>
+        <a href="{{ route('emprestimos.create') }}" class="btn btn-primary align-self-start">Realizar Empréstimo</a>
+        <a href="{{ route('livros.listar') }}" class="btn btn-primary align-self-start">Visualizar Livros</a>
+        <a href="{{ route('emprestimos.emitir') }}" class="btn btn-primary align-self-start">Gerar PDF dos Empréstimos</a>
+    </div>
 @endsection
